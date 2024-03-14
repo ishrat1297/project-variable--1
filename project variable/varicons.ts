c4787561a8abdefbcb6ce6f66895ccd6941b7f41
afshan feroz <160025739+ishrat1297@users.noreply.github.com>
2024-03-14 14:15:51 +0500

    Add files via upload

---
+++ project variable/varicons.ts
@@ -0,0 +1,132 @@
+// FIRST NAME
+let firstName:string="afshan";
+console.log(firstName);
+
+// LAST NAME
+
+let last_name:string="feroz";
+console.log(last_name);
+ 
+// FULL NAME
+ 
+let FullName:string="afshanferoz";
+console.log(FullName);
+
+// QUALIFICATION
+
+let qualification:string="B.com";
+console.log(qualification);
+
+// CITY
+
+let city:string="karachi";
+console.log(city);
+
+// COUNTRY
+
+let country:string="pakistan";
+console.log(country);
+const dob:number=12011997;
+console.log(dob);
+const sibling:number=6;
+console.log(sibling);
+
+// HOBBY
+
+let hobby:string="reading";
+console.log(hobby);
+
+// PROFESSION
+
+let profession:string="teaching";
+console.log(profession);
+
+// AREA
+
+let area:string='usmanabad';
+console.log(area);
+
+// TOWN
+
+let town:string="lyari";
+console.log(town);
+
+// FAVOURITE SUBJECT
+
+let favoriteSubject:string="math";
+console.log(favoriteSubject);
+const religion:string="islam";
+console.log(religion);
+
+// FAVOURITE GAME
+
+let favouritegame:string="cricket";
+console.log(favouritegame);
+favouritegame="soccer";
+console.log(favouritegame);
+favouritegame="volleyball";
+console.log(favouritegame);
+
+// FAVOURITE WRITER
+
+let favouriteWriter:string="shakespear";
+console.log(favouriteWriter);
+
+// FAVOURITE SCHOLER
+
+let favouritescholer:string="israrahmed";
+console.log(favouritescholer);
+
+// NICKNAME
+
+let nickNAME:string="aena";
+console.log(nickNAME);
+
+// HEIGHT
+
+const height:number=5.4;
+console.log(height);
+
+// FAVOURITE MOVIE
+
+let favouritemovie:string="tare zameen per";
+console.log(favouritemovie);
+
+// FAVOURITE SONG
+
+let favouritesong:string="who said";
+console.log(favouritesong);
+favouritesong="flowers";
+console.log(favouritesong);
+
+// FAVOURITE COLOUR
+
+let favouritecolour:string="green";
+
+// FAVOURITE PICNIC PLACE
+
+let favouritepicnicplace:string='seaside';
+
+// MOTHER NAME
+
+let mothername:string="ishrat"
+console.log(favouritecolour)
+console.log(favouritepicnicplace)
+console.log(mothername)
+
+// FAVOURITE QOUTE
+
+let favouritequote:string="do the best until you know better.then when you know better, do better "
+
+// FAVOURITE WEATHER
+
+let favouriteweather:string='rain'
+
+// 2ND FAVOURITE QUOTE
+
+favouritequote="success is not final failure is not fatal it is the courage to continue that count"
+console.log(favouritequote)
+console.log(favouriteweather)
+console.log(favouritequote)
+
+
